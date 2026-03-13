925184a633578e9ef4abad0daa17403acd317395
BearcatTanner <Tannerdpatrick06@gmail.com>
2026-03-13 11:57:05 -0400

    Add project files.

---
+++ IT3048FinalNationalParksApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IT3048FinalNationalParksApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IT3048FinalNationalParksApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IT3048FinalNationalParksApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
