8e15a175333ab76f17fb06f4330c7daca27e65f5
Peter Nguyen <nguye5he@mail.uc.edu>
2026-04-09 11:08:15 -0400

    Update AuthShell.xaml.cs

--- IT3048FinalNationalParksApp/AuthShell.xaml.cs
+++ IT3048FinalNationalParksApp/AuthShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace IT3048FinalNationalParksApp
+﻿using IT3048FinalNationalParksApp.Auth;
+
+namespace IT3048FinalNationalParksApp;
+
+public partial class AuthShell : Shell
 {
-    public partial class AuthShell : Shell
+    public AuthShell()
     {
-        public AuthShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        // Register routes so Shell.Current.GoToAsync(nameof(...)) can find the pages
+        Routing.RegisterRoute(nameof(SignUpPage), typeof(SignUpPage));
+        Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
     }
 }
