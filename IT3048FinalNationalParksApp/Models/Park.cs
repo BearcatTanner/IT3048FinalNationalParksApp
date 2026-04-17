f83ee3de5b9f7d1a731ba5c8879eb7c795782cb6
Peter Nguyen <nguye5he@mail.uc.edu>
2026-04-17 19:58:51 -0400

    added stamp images and created park table

---
+++ IT3048FinalNationalParksApp/Models/Park.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace IT3048FinalNationalParksApp.Models;
+
+public class Park
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string ParkCode { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string StampImage { get; set; } = string.Empty;
+}
---
+++ IT3048FinalNationalParksApp/Services/APIService.cs
@@ -0,0 +1,57 @@
+﻿using IT3048FinalNationalParksApp.Models;
+using Newtonsoft.Json;
+
+namespace IT3048FinalNationalParksApp.Services
+{
+    internal class APIService
+    {
+        public async Task<Parks> GETParksOH()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync
+                ($"https://developer.nps.gov/api/v1/parks?stateCode=OH&" +
+                $"&api_key=tanbDLVroQ7S3zgYdIiTSD0XvXJFFapIGmghgRzK");
+            return JsonConvert.DeserializeObject<Parks>(response);
+        }
+        public async Task<Parks> GETParksQueryOH(string query)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync
+                ($"https://developer.nps.gov/api/v1/parks?stateCode=OH&q=" +
+                $"{query}&api_key=tanbDLVroQ7S3zgYdIiTSD0XvXJFFapIGmghgRzK");
+            return JsonConvert.DeserializeObject<Parks>(response);
+        }
+        public async Task<Parks> GETParksIN()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync
+                ($"https://developer.nps.gov/api/v1/parks?stateCode=IN&" +
+                $"&api_key=tanbDLVroQ7S3zgYdIiTSD0XvXJFFapIGmghgRzK");
+            return JsonConvert.DeserializeObject<Parks>(response);
+        }
+        public async Task<Parks> GETParksQueryIN(string query)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync
+                ($"https://developer.nps.gov/api/v1/parks?stateCode=IN&q=" +
+                $"{query}&api_key=tanbDLVroQ7S3zgYdIiTSD0XvXJFFapIGmghgRzK");
+            return JsonConvert.DeserializeObject<Parks>(response);
+        }
+        public async Task<Parks> GETParksKy()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync
+                ($"https://developer.nps.gov/api/v1/parks?stateCode=IN&" +
+                $"&api_key=tanbDLVroQ7S3zgYdIiTSD0XvXJFFapIGmghgRzK");
+            return JsonConvert.DeserializeObject<Parks>(response);
+        }
+        public async Task<Parks> GETParksQueryKY(string query)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync
+                ($"https://developer.nps.gov/api/v1/parks?stateCode=IN&q=" +
+                $"{query}&api_key=tanbDLVroQ7S3zgYdIiTSD0XvXJFFapIGmghgRzK");
+            return JsonConvert.DeserializeObject<Parks>(response);
+        }
+    }
+}
