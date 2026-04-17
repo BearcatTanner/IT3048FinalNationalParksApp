220b07ab33cfa2c807bc94c4bc36038ea66c35b7
winnieduong <khanhuyenhduong@gmail.com>
2026-04-16 23:08:19 -0400

    save user information to local database using db browser for sqlite

--- IT3048FinalNationalParksApp/MauiProgram.cs
+++ IT3048FinalNationalParksApp/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Maui;
 using IT3048FinalNationalParksApp.Auth;
 using IT3048FinalNationalParksApp.Services;
+using IT3048FinalNationalParksApp.Views; 
 using Microsoft.Extensions.Logging;
 
 namespace IT3048FinalNationalParksApp
@@ -10,7 +11,6 @@ namespace IT3048FinalNationalParksApp
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-
             builder
                 .UseMauiApp<App>()
                 .UseMauiCommunityToolkit()
@@ -20,14 +20,20 @@ namespace IT3048FinalNationalParksApp
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            // Register Services
             builder.Services.AddSingleton<DatabaseService>();
+
+            // Register ViewModels
+            builder.Services.AddTransient<SignUpViewModel>();
+            builder.Services.AddTransient<LogInViewModel>();
+
+            // Register Pages
             builder.Services.AddTransient<SignUpPage>();
             builder.Services.AddTransient<LoginPage>();
 
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
-
             return builder.Build();
         }
     }
